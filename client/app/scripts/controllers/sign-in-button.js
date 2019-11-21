23c32bd2c2ac4f94aa76c68df56de5868d267f58
Xianfeng Yuan <xianfeng.yuan@menlosecurity.com>
2019-11-20 23:28:19 -0800

    add login function

--- client/app/scripts/controllers/sign-in-button.js
+++ client/app/scripts/controllers/sign-in-button.js
@@ -9,6 +9,19 @@
  */
 angular.module('clientApp')
   .controller('SignInButtonCtrl', function($scope, $auth, $location, ngNotify){
+    $scope.login = function() {
+      $auth.login($scope.user)
+      .then(function(response) {
+        $location.path('/');
+      })
+      .catch(function(response){
+        ngNotify.set('Login failed.', {
+          type: 'error',
+          sticky: true,
+          button: true,
+        });
+      });
+    };
     $scope.authenticate = function() {
       $auth.authenticate('github')
       .then(function(response) {
